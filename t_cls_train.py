a671787ef806fa686cec8ecdd4b7025165cf709b
sota <sota@gmail.com>
2020-12-01 14:48:39 +0900

    merge

--- t_cls_train.py
+++ t_cls_train.py
@@ -259,8 +259,8 @@ class WeatherTransfer(object):
         # fake_feat = fake_res[3]
         fake_c_out = self.classifier(fake_out)
 
-        # fake_res = self.discriminator(fake_out, F.softmax(fake_c_out, dim=1))
-        # fake_d_out = fake_res[0]
+        fake_res = self.discriminator(fake_out, F.softmax(fake_c_out, dim=1))
+        fake_d_out = fake_res[0]
 
         if self.args.weather_loss != 'CE':
             fake_c_out = F.softmax(fake_c_out, dim=1)
@@ -323,8 +323,8 @@ class WeatherTransfer(object):
         # for fake
         fake_out = self.inference(images, r_labels)
         # -- test --- #
-        # fake_c_out = self.classifier(fake_out).detach()
-        # fake_d_out = self.discriminator(fake_out.detach(), F.softmax(fake_c_out, dim=1))[0]
+        fake_c_out = self.classifier(fake_out).detach()
+        fake_d_out = self.discriminator(fake_out.detach(), F.softmax(fake_c_out, dim=1))[0]
         # ----------- #
         # fake_d_out = self.discriminator(fake_out.detach(), r_labels)[0]
 
