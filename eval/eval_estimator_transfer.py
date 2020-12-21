7d6ae9501fbf19818569cace8ba98e3ff9f18623
temp <temp@gmail.com>
2020-12-21 07:37:01 +0000

    update

--- eval/eval_estimator_transfer.py
+++ eval/eval_estimator_transfer.py
@@ -11,8 +11,7 @@ from tqdm import trange, tqdm
 from torchvision.utils import save_image
 import matplotlib.pyplot as plt
 
-from sklearn.metrics import classification_report, confusion_matrix
-import seaborn as sns
+from sklearn.metrics import r2_score, mean_absolute_error, mean_squared_error
 
 parser = argparse.ArgumentParser()
 parser.add_argument('--gpu', type=str)
@@ -45,20 +44,28 @@ from dataset import FlickrDataLoader
 from cunet import Conditional_UNet
 
 
-def eval_est_trasnfer(batch, b_sig, r_sig, l1_li):
+def eval_est_trasnfer(batch, r_sig):
+    y_true = []
+    y_pred = []
     for j in range(bs):
         with torch.no_grad():
             ref_sig_expand = torch.cat([r_sig[j]] * bs).view(-1, len(cols))
             out = transfer(batch, ref_sig_expand)
 
+            y_true.append(ref_sig_expand.to('cpu'))
             pred_sig = estimator(out)
+            y_pred.append(pred_sig.to('cpu'))
             # l1 = torch.abs(pred_sig - ref_sig_expand)
-            l1 = pred_sig - ref_sig_expand
-            l1_li = np.append(l1_li, torch.mean(l1, dim=0).cpu().numpy().reshape(1, -1), axis=0)
+            # l1 = pred_sig - ref_sig_expand
+            # mse = F.mse_loss(pred_sig, ref_sig_expand, reduction='none')
+            # l1_li = np.append(l1_li, torch.mean(l1, dim=0).cpu().numpy().reshape(1, -1), axis=0)
+            # mse_li = np.appenc(mse_li, torch.mean(mse, dim=0).cpu.numpy().reshape(1, -1), axis=0)
             # [save_image(output, os.path.join(args.output_dir,
             #     '{}_t-{}_r-{}.jpg'.format('gt', b_photos[j], r_photos[i])), normalize=True)
             #     for j, output in enumerate(out)]
-    return l1_li
+    y_true_ = torch.cat(y_true, dim=0)
+    y_pred_ = torch.cat(y_pred, dim=0)
+    return y_true_, y_pred_
 
 
 if __name__ == '__main__':
@@ -104,6 +111,7 @@ if __name__ == '__main__':
     transfer = Conditional_UNet(num_classes=len(cols))
     sd = torch.load(args.cp_path)
     transfer.load_state_dict(sd['inference'])
+    transfer.eval()
 
     estimator = torch.load(args.estimator_path)
     estimator.eval()
@@ -112,32 +120,52 @@ if __name__ == '__main__':
     estimator.cuda()
 
     bs = args.batch_size
-    l1_li = np.empty((0, len(cols)))
+    y_true_l = []
+    y_pred_l = []
     for i, (data, rnd) in tqdm(enumerate(zip(loader, random_loader)), total=len(df)//bs):
         batch = data[0].to('cuda')
         batch = dataset.transform(batch)
-        b_sig = data[1].to('cuda')
+        # b_sig = data[1].to('cuda')
         # r_batch  = rnd[0].to('cuda')
         r_sig = rnd[1].to('cuda')
 
-        l1_li = eval_est_trasnfer(batch, b_sig, r_sig, l1_li)
+        y_true, y_pred = eval_est_trasnfer(batch, r_sig)
+        y_true_l.append(y_true)
+        y_pred_l.append(y_pred)
 
-    ave_l1 = np.mean(l1_li, axis=0)
-    std_l1 = np.std(l1_li, axis=0)
+    y_true_l = torch.cat(y_true_l, dim=0).numpy()
+    y_pred_l = torch.cat(y_pred_l, dim=0).numpy()
 
-    print(cols)
-    print('l1')
-    print(ave_l1)
-    print((ave_l1 * df_std))
-    print('l1 std')
-    print(std_l1)
-    print((std_l1 * df_std))
+    r2 = r2_score(y_true_l, y_pred_l)
+    mae = mean_absolute_error(y_true_l, y_pred_l)
+    mse = mean_squared_error(y_true_l, y_pred_l)
 
-    fig = plt.figure(figsize=(40, 10))
-    for i, col in enumerate(cols):
-        ax = fig.add_subplot(2, 4, i)
-        ax.hist(l1_li[:, i], bins=100)
-        ax.set_xlabel('{}_L1'.format(col))
+    print("r2 score = {}".format(r2))
+    print("mae score = {}".format(mae))
+    print("mse score = {}".format(mse))
 
-    fig.savefig(os.path.join(save_path, '{}_eval_result.jpg'.format(col)))
-    plt.clf()
+    # ave_l1 = np.mean(l1_li, axis=0)
+    # std_l1 = np.std(l1_li, axis=0)
+    # ave_mse = np.mean(mse_li, axis=0)
+
+    # print(cols)
+    # print('l1')
+    # print(ave_l1)
+    # print((ave_l1 * df_std))
+    # print('l1 std')
+    # print(std_l1)
+    # print((std_l1 * df_std))
+
+    # print('each mse')
+    # print(ave_mse)
+    # print('all mean mse')
+    # print(np.mean(mse_li))
+
+    # fig = plt.figure(figsize=(40, 10))
+    # for i, col in enumerate(cols):
+    #     ax = fig.add_subplot(2, 4, i)
+    #     ax.hist(l1_li[:, i], bins=100)
+    #     ax.set_xlabel('{}_L1'.format(col))
+
+    # fig.savefig(os.path.join(save_path, '{}_eval_result.jpg'.format(col)))
+    # plt.clf()
