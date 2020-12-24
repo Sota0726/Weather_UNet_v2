898885703922cb11e70811b10cbc747a5e636113
temp <temp@gmail.com>
2020-12-24 17:50:11 +0900

    to merge

--- inference/inf_transfer_e.py
+++ inference/inf_transfer_e.py
@@ -11,13 +11,13 @@ from glob import glob
 from torchvision.utils import save_image
 
 parser = argparse.ArgumentParser()
-parser.add_argument('--gpu', type=str, default='3')
+parser.add_argument('--gpu', type=str, default='2')
 parser.add_argument('--image_root', type=str,
                     default='/mnt/HDD8T/takamuro/dataset/photos_usa_224_2016-2017')
 parser.add_argument('--pkl_path', type=str,
-                    default='/mnt/fs2/2019/Takamuro/m2_research/flicker_data/wwo/2016_17/lambda_0/outdoor_all_dbdate_wwo_weather_2016_17_delnoise_WoPerson_sky-10_L-05_50testImgs.pkl')
+                    default='/mnt/fs2/2019/Takamuro/m2_research/flicker_data/wwo/2016_17/lambda_0/outdoor_all_dbdate_wwo_weather_2016_17_delnoise_WoPerson_sky-10_L-05.pkl')
 parser.add_argument('--cp_path', type=str,
-                    default='cp/transfer/1204_cUNet_w-e_res101-1203L05e15_SNDisc_sampler-False_GDratio1-8_adam-b10.5-b20.9_lr-0.0001_bs-24_ne-150/cUNet_est_e0110_s1168000.pt')
+                    default='/mnt/fs2/2019/Takamuro/m2_research/weather_transferV2/cp/transfer/est/1204_cUNet_w-e_res101-1203L05e15_SNDisc_sampler-False_GDratio1-8_adam-b10.5-b20.9_lr-0.0001_bs-24_ne-150/cUNet_est_e0134_s1432000.pt')
 parser.add_argument('--estimator_path', type=str,
                     default='/mnt/fs2/2019/Takamuro/m2_research/weather_transferV2/cp/estimator/'
                     'est_res101-1203_sampler_pre_WoPerson_sky-10_L-05/est_resnet101_15_step62240.pt')
@@ -25,7 +25,8 @@ parser.add_argument('--input_size', type=int, default=224)
 parser.add_argument('--batch_size', type=int, default=10)
 parser.add_argument('--num_workers', type=int, default=8)
 parser.add_argument('--image_only', action='store_true')
-args = parser.parse_args()
+args = parser.parse_args(['--pkl_path', '/mnt/fs2/2019/Takamuro/m2_research/flicker_data/wwo/2016_17/lambda_0/for_inf_10imgs.pkl'])
+# args = parser.parse_args()
 
 os.environ['CUDA_DEVICE_ORDER'] = "PCI_BUS_ID"
 os.environ['CUDA_VISIBLE_DEVICES'] = args.gpu
@@ -80,7 +81,8 @@ if __name__ == '__main__':
             # sampler=ImbalancedDatasetSampler(dataset),
             batch_size=args.batch_size,
             num_workers=args.num_workers,
-            drop_last=True
+            drop_last=True,
+            shuffle=True
             )
     random_loader = torch.utils.data.DataLoader(
             dataset,
@@ -88,13 +90,14 @@ if __name__ == '__main__':
             batch_size=args.batch_size,
             num_workers=args.num_workers,
             drop_last=True,
-            shuffle=True
+            # shuffle=True
             )
 
     # load model
     transfer = Conditional_UNet(len(cols))
     sd = torch.load(args.cp_path)
     transfer.load_state_dict(sd['inference'])
+    transfer.eval()
 
     estimator = torch.load(args.estimator_path)
     estimator.eval()
@@ -108,7 +111,7 @@ if __name__ == '__main__':
 
     save_path = os.path.join('/mnt/fs2/2019/Takamuro/m2_research/weather_transferV2/results/eval_transfer', 'est',
                              args.cp_path.split('/')[-2],
-                             args.cp_path.split('/')[-1].split('.pt')[0], '50x50')
+                             args.cp_path.split('/')[-1].split('.pt')[0], '10x10')
     os.makedirs(save_path, exist_ok=True)
     for k, (data, rnd) in tqdm(enumerate(zip(loader, random_loader)), total=len(df_sep)//bs):
         batch = data[0].to('cuda')
@@ -123,7 +126,8 @@ if __name__ == '__main__':
         r_photos = rnd[2]
 
         blank = torch.zeros_like(batch[0]).unsqueeze(0)
-        ref_imgs = torch.cat([blank] + list(torch.split(r_batch, 1)), dim=3)
+        ref_imgs = torch.cat([blank] + list(torch.split(r_batch, 1)), dim=3).to('cpu')
+        save_image(ref_imgs, fp=os.path.join(save_path, 'ref.jpg'), normalize=True, scale_each=True, nrow=1)
         out_l = []
         for i in tqdm(range(bs)):
             with torch.no_grad():
@@ -134,6 +138,7 @@ if __name__ == '__main__':
                  '{}-{}_r-{}.jpg'.format('gt', b_photos[j], r_photos[i])), normalize=True, scale_each=True)
                  for j, output in enumerate(out)]
 
-        io_im = torch.cat([batch] + out_l, dim=3)
+        io_im = torch.cat([batch] + out_l, dim=3).to('cpu')
+        [save_image(_, fp=os.path.join(save_path, 'row_{}.jpg'.format(i)), normalize=True, scale_each=True) for i, _ in enumerate(io_im)]
         tab_im = torch.cat([ref_imgs, io_im], dim=0)
         save_image(tab_im, fp=os.path.join(save_path, '{}.jpg'.format(k)), normalize=True, scale_each=True, nrow=1)
